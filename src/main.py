1f00199045f20ca513cdede4eaead675ecb097a3
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2023-06-07 12:10:48 +0200

    feat: Migrate todo table

--- src/main.py
+++ src/main.py
@@ -1,28 +1,35 @@
-from flask import Flask, request
-from ariadne import gql, QueryType
-import os
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import uuid
+import os
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-type_defs = gql("""
-    type Query {
-        hello: String!
-    }
-""")
+class Todo(db.Model):
+    id = db.Column(db.String(36), primary_key=True, default=str(uuid.uuid4()))
+    title = db.Column(db.String(80), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False)
 
-query = QueryType()
-
-
-
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "completed": self.completed
+        }
 
 @app.route("/", methods=['GET'])
 def hello():
-    return 'Hello, Wold!'
+    return 'Hello, World!'
 
 @app.route("/graphql", methods=['POST'])
 def graphql():
     return 'Hello, World!'
+
+if __name__ == "__main__":
+    app.run()
